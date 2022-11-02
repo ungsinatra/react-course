ba64d24fbaeabc76bc734f52a06b7d9d81503c62
ungsinatra <mamirov.21@mail.ru>
2022-11-02 23:43:39 +0300

    add some ui component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 // import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button /Mybutton";
+import MyInput from "./components/UI/Input/MyInput";
 import './styles/App.css';
 
 
@@ -13,9 +15,38 @@ function App() {
     {id:3,title:'JavaScript 3',desc:'Путь к React'}
   ])
 
+
+  const [title,setTitle] = useState('');
+  const [decs,setDesc] = useState('');
+
+  const titleHandler = (evt) => {
+    setTitle(evt.target.value);
+  }
+
+  const decsHandler = (evt) => {
+    setDesc(evt.target.value)
+    console.log(decs)
+  }
+
   return (
     <div className="App">
+      <form>
+      <MyInput 
+        type = 'text' 
+        value = {title} 
+        placeholder="Название поста"
+        onChange = {titleHandler}
       
+      />
+      <MyInput 
+      value = {decs}
+      type = 'text' 
+      placeholder="Описание поста" 
+      onChange = {decsHandler}
+      />  
+      <MyButton  type="button">Создать пост</MyButton>
+      </form>
+
     
       <PostList posts = {posts} title = 'Список постов' key = {posts.id}/>
  
