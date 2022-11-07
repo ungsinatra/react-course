5eedf8eea3ab773dd40e50c4a7b96ccd5e71a83b
ungsinatra <mamirov.21@mail.ru>
2022-11-07 11:57:27 +0300

    remove

--- src/components/testsss/LogInButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function LogInButton(props) {
-    return(
-        <button onClick={props.onClick}>Войти</button>
-    )
-}
-
-export default LogInButton;
--- src/components/testsss/LogOutButton.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function LogOutButton(props) {
-    return (
-        <button onClick={props.onClick}>Выйти</button>
-    )
-}
-
-export default LogOutButton;
